112a330388c0d7180a05e59832de93ea8258a3e8
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-10-31 10:43:56 +0100

    chore: add loading spinner

--- view/MainWindow.py
+++ view/MainWindow.py
@@ -1,7 +1,7 @@
-from textual import on
+from textual import on, work
 from textual.app import App, ComposeResult
 from textual.containers import Vertical
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, LoadingIndicator
 from lib.GarminRepository import GarminRepository
 from ui.ActivityFilter import ActivityFilter
 from ui.ActivityTable import ActivityTable
@@ -33,6 +33,7 @@ class MainWindow(App):
         yield Header()
         yield Vertical(
             ActivityFilter(),
+            LoadingIndicator(),
             ActivityTable(),
         )
         yield Footer()
@@ -47,17 +48,29 @@ class MainWindow(App):
         if event.validation_result and event.validation_result.is_valid:
             self.notify(f"End Date: {event.value}")
 
-    async def on_ready(self) -> None:
-        await self.load_activities()
+    def on_ready(self) -> None:
+        self._show_loading_indicator(True)
+        self.update_activities("2025-10-01")
 
-    async def load_activities(self):
-        table = self.query_one(ActivityTable)
-        for act in self.REPOSITORY.get_activities("2025-10-01"):
-            table.add_row(
-                act.start_time,
-                act.id,
-                act.formatted_visibility,
-                act.formatted_name,
-                act.formatted_distance,
-                act.formatted_duration,
-            )
+    def _show_loading_indicator(self, display: bool):
+        self.query_one(LoadingIndicator).display = display
+
+    @work(exclusive=True, thread=True)
+    async def update_activities(self, start_date: str, end_date: str | None = None):
+        self.call_from_thread(self._show_loading_indicator, True)
+        try:
+            table = self.query_one(ActivityTable)
+            table.clear()
+            for act in self.REPOSITORY.get_activities(start_date, end_date):
+                table.add_row(
+                    act.start_time,
+                    act.id,
+                    act.formatted_visibility,
+                    act.formatted_name,
+                    act.formatted_distance,
+                    act.formatted_duration,
+                )
+        except Exception as e:
+            self.app.notify(f"Error fetching activities: {e}", severity="error")
+        finally:
+            self.call_from_thread(self._show_loading_indicator, False)
