907aa92a523a285ae2b404a1f3b73fbfa55e5f15
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-11-02 21:47:34 +0100

    chore: wip

--- client/RaceID.py
+++ client/RaceID.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
 import requests
-from typing import Optional, Dict, Any
+from dataclasses import dataclass
+from typing import Literal, Optional, Any
 
 
 @dataclass
@@ -17,78 +17,65 @@ class RaceID:
     BASE_API_URL = "https://api.raceid.com/api/v1/web"
     bearer_token: Optional[str] = None
 
-    def login(self, username: str, password: str):
-        url = f"{self.BASE_API_URL}/user/login"
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-        }
-        payload = {
-            "email": username,
-            "password": password,
-        }
+    def auth(self, username: str, password: str):
+        json = {"email": username, "password": password}
+        response = self._make_request("POST", "/user/login", json=json)
+        self.bearer_token = response.get("data", {}).get("token")
 
-        response = requests.post(url, json=payload, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        self.bearer_token = data.get("data", {}).get("token")
-
-    def get_results(self, id: str, page: int = 1, limit: int = 100):
+    def get_results(self, id: str, page: int = 1, limit: int = 100) -> list[Result]:
         params = {"page": page, "limit": limit}
-        return self._make_request("GET", f"/racers/{id}/segments", params=params)
-
-    def log_result(self, id: str, log: Result) -> Optional[Dict[str, Any]]:
-        data = {
-            "activity_link": log.activity_link,
-            "distance_logged": log.distance_logged,
-            "distance_logged_at": log.distance_logged_at,
-            "time_result": log.time_result,
-        }
+        response = self._make_request("GET", f"/racers/{id}/segments", params=params)
 
         results = []
-        for response in self._make_request("POST", f"/racers/{id}/segments", data=data):
+        for res in response.get("data", {}):
             result = Result(
-                id=response["id"],
-                result_id=response["result_id"],
-                activity_link=response["activity_link"],
-                distance_logged=response["distance_logged"],
-                distance_logged_at=response["distance_logged_at"],
-                time_result=response["time_result"],
+                activity_link=res["activity_link"],
+                distance_logged=res["distance_logged"],
+                distance_logged_at=res["distance_logged_at"],
+                time_result=res["time_result"],
+                result_id=res["result_id"],
+                id=res["id"],
             )
             results.append(result)
         return results
 
-    def _make_request(
-        self,
-        method: str,
-        endpoint: str,
-        params: Any = {},
-        data: Any = {},
-        with_auth: bool = True,
-    ) -> Dict[str, Any]:
-        url = f"{self.BASE_API_URL}{endpoint}"
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
+    def log_result(self, id: str, result: Result):
+        json = {
+            "activity_link": result.activity_link,
+            "distance_logged": result.distance_logged,
+            "distance_logged_at": result.distance_logged_at,
+            "time_result": f"{result.time_result}.000",
         }
 
+        endpoint = f"/racers/{id}/segments"
+        return self._make_request("POST", endpoint=endpoint, json=json)
+
+    def _make_request(
+        self,
+        method: Literal["GET", "POST"],
+        endpoint: str,
+        params: Any = {},
+        json: Any = {},
+        with_auth: bool = True,
+    ) -> Any:
+        url = f"{self.BASE_API_URL}{endpoint}"
+        headers = {"accept": "application/json", "content-type": "application/json"}
         if with_auth:
             headers["x-authorization"] = f"Bearer {self.bearer_token}"
 
         try:
-            if method == "GET":
-                response = requests.get(url, headers=headers, params=params)
-            elif method == "POST":
-                response = requests.post(url, headers=headers, json=data)
-            else:
-                raise ValueError(f"Invalid method: {method}")
+            match (method):
+                case "GET":
+                    response = requests.get(url, headers=headers, params=params)
+                case "POST":
+                    response = requests.post(url, headers=headers, json=json)
 
             # print(f"method: {method}")
             # print(f"respone: {response.status_code}")
             # print(f"data: {response.json()}")
 
             response.raise_for_status()
-            return response.json().get("data", {})
+            return response.json()
 
         except requests.RequestException as ex:
             print(ex)
@@ -97,14 +84,17 @@ class RaceID:
 
 if __name__ == "__main__":
     client = RaceID()
-    client.login("sebastian.ullrich@deepsource.de", "hqg.ufg@vnd8EUZ2jxt")
+    client.auth("sebastian.ullrich@deepsource.de", "hqg.ufg@vnd8EUZ2jxt")
 
-    log = Result(
+    result = Result(
         activity_link="https://connect.garmin.com/modern/activity/20870918926",
         distance_logged=33740,
         distance_logged_at="2025-11-02",
         time_result="01:07:17",
     )
 
-    print(client.get_results("995659"))
-    # client.log_result(id="995659", log=log)
+    # client.log_result(id="995659", result=result)
+    for result in client.get_results("995659"):
+        print(
+            f"{result.id} {result.result_id} {result.time_result} {result.activity_link}"
+        )
